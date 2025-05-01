a2e428d8d23657b66d08234d37636610babb5e0a
mahmudurmahid <m.mahid@icloud.com>
2025-05-01 15:53:25 +0100

    added menu bar to GUI

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow
+from PyQt6.QtGui import QAction
 import sys
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Student Management System")
 
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+        about_action.setMenuRole(QAction.MenuRole.NoRole) # Allows help menu to be displayed
+        
 
 
 app = QApplication(sys.argv)
